eb36788c087cf9dd405c8192b78b6b961d717d69
Pallavi2812 <pallavim2812@gmail.com>
2024-10-29 16:40:42 +0530

    Update main.py

--- pyscripts/main.py
+++ pyscripts/main.py
@@ -1,5 +1,4 @@
-        from pyscript import Element
-
-        # Function to print "Hello, World!" (it must accept the event argument)
-        def print_hello(event):
-            Element("output").write("Hello, World!")
+from pyscript import Element
+# Function to print "Hello, World!" (it must accept the event argument)
+def print_hello(event):
+        Element("output").write("Hello, World!")
