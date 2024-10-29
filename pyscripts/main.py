ba2e3d4558b222cc5b1ae9727c75f43ded99ae05
Pallavi2812 <pallavim2812@gmail.com>
2024-10-29 16:36:45 +0530

    Update main.py

--- pyscripts/main.py
+++ pyscripts/main.py
@@ -3,6 +3,3 @@
         # Function to print "Hello, World!" (it must accept the event argument)
         def print_hello(event):
             Element("output").write("Hello, World!")
-
-        # Bind the print_hello function to the button's click event
-        Element("helloBtn").element.onclick = print_hello
