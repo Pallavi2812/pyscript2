0c08c6f898bbf22dea6f2f41a1f8b89757420483
Pallavi2812 <pallavim2812@gmail.com>
2024-11-13 12:26:00 +0530

    Update main.py

--- pyscripts/main.py
+++ pyscripts/main.py
@@ -1,24 +1,3 @@
-import subprocess
-import sys
-
-# Function to install packages
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-# Install required packages
-install("azure-identity")
-install("azure-keyvault-secrets")
-
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
-
-vault_url = "https://test-pyscript-new.vault.azure.net/"
-secret_name = "test"
-
-# Authenticate to Azure Key Vault
-credential = DefaultAzureCredential()
-client = SecretClient(vault_url=vault_url, credential=credential)
-
-# Fetch the secret value
-secret = client.get_secret(secret_name)
-print(f"The value of the secret '{secret_name}' is: {secret.value}")
+from pyscript import Element
+def print_hello(event):
+    Element('output').write('Hello, World!')
